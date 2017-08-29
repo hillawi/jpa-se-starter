3a25bb0a43eeabaacf7a560b0b7ac41e841335be
Ahmed Abdeen <ahmed.abdeen@tobania.be>
2017-08-29 22:52:20 +0200

    Ignoring target and shade-plugin output

--- target/classes/queries.sql
+++
@@ -1,4 +0,0 @@
-select c.f_name || ' ' || c.l_name, p.dsc, o.order_id, o.creation_date, o.modification_date, o.status from product p, cart_order o, order_product op, customer c
-where op.order_id = o.order_id
-and op.product_id = p.product_id
-and o.customer_id = c.customer_id;
